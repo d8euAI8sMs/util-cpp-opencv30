df776f6ef8c8137841c291a3a5cdd17d64afec8a
kalaider <a.kalaider@yandex.ru>
2019-05-06 19:54:04 +0300

    replace headers with cmake build results including contrib headers

--- include/opencv2/cvconfig.h
+++ include/opencv2/cvconfig.h
@@ -1,5 +1,5 @@
 /* OpenCV compiled as static or dynamic libs */
-/* #undef BUILD_SHARED_LIBS */
+#define BUILD_SHARED_LIBS
 
 /* Compile for 'real' NVIDIA GPU architectures */
 #define CUDA_ARCH_BIN ""
@@ -26,10 +26,10 @@
 /* #undef HAVE_CARBON */
 
 /* AMD's Basic Linear Algebra Subprograms Library*/
-#define HAVE_CLAMDBLAS
+/* #undef HAVE_CLAMDBLAS */
 
 /* AMD's OpenCL Fast Fourier Transform Library*/
-#define HAVE_CLAMDFFT
+/* #undef HAVE_CLAMDFFT */
 
 /* Clp support */
 /* #undef HAVE_CLP */
